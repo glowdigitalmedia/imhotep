936413966c54473bdfccb8d735eee0637bebba77
Konrad Korzel <konrad@korzel.com>
2014-11-14 18:08:17 +0000

    Downgrade requests requirement, bump up version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/justinabrahms/imhotep',
     license='MIT',
     author='Justin Abrahms',
     author_email='justin@abrah.ms',
     description='A tool to pipe linters into code review',
-    install_requires=['requests==2.3.0'],
+    install_requires=['requests==2.2.1'],
     extras_require={'pylint': 'PyLint'},
     entry_points = {
         'console_scripts': [

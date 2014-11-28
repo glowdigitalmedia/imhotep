b5d8fd692d389e038ed384181c7a00b3e75ef390
Konrad Korzel <konrad.korzel@gmail.com>
2014-11-28 16:32:29 +0000

    Update requirements and setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name='imhotep',
     version='0.1.1',
     packages=find_packages(),
-    url='https://github.com/justinabrahms/imhotep',
+    url='https://github.com/glowdigitalmedia/imhotep',
     license='MIT',
-    author='Justin Abrahms',
-    author_email='justin@abrah.ms',
-    description='A tool to pipe linters into code review',
-    install_requires=['requests==2.2.1'],
+    author='Justin Abrahms, Konrad Korzel',
+    author_email='justin@abrah.ms, konrad.korzel@gmail.com',
+    description='A fork of Justin Abrahms tool to pipe linters into code review by ',
+    install_requires=['requests==2.2.1', 'pep8'],
     extras_require={'pylint': 'PyLint'},
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'imhotep = imhotep.main:main',
         ],

6d888d0d6472a09fcb2bc6caa5d29508ee58fc0f
Konrad Korzel <konrad.korzel@gmail.com>
2014-11-28 16:34:20 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     url='https://github.com/glowdigitalmedia/imhotep',
     license='MIT',
     author='Justin Abrahms, Konrad Korzel',
     author_email='justin@abrah.ms, konrad.korzel@gmail.com',
     description='A fork of Justin Abrahms tool to pipe linters into code review by ',
-    install_requires=['requests==2.2.1', 'pep8'],
+    install_requires=['requests==2.2.1'],
     extras_require={'pylint': 'PyLint'},
     entry_points={
         'console_scripts': [
